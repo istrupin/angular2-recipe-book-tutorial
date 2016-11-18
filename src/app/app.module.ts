987506f985d6e2a244d4e3e06d723cb6f65c9b8f
Strupinskiy <IStrupin@russellinvestments.com>
2016-11-18 08:40:58 -0800

    Added Add button and set up for template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -41,6 +41,7 @@ import {routing} from './app.routing';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     routing
   ],
--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient';
+import { ShoppingListService} from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  isAdd = true;
+  item: Ingredient;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(ingredient: Ingredient){
+    if(!this.isAdd) {
+      //edit
+    } else {
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+    }
+
   }
 
 }
