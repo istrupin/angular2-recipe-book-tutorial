ed1bf2cd8cc3be1d6f3b9e685eca0f56b8c3d1a4
Strupinskiy <IStrupin@russellinvestments.com>
2016-11-21 16:07:12 -0800

    Adding for for editing and adding recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from "../recipe.service";
+import { Subscription } from "rxjs/Rx";
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private recipeIndex: number;
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, 
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
+    let isNew = true;
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+       
+        if (params.hasOwnProperty('id')) {
+          isNew = false;
+          this.recipeIndex = +params['id']; //+converts the string into a number
+        } else {
+          isNew = true;
+        }
+        console.log(isNew);
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/Ingredient';
 import { ShoppingListService} from './shopping-list.service';
 
@@ -6,23 +6,42 @@ import { ShoppingListService} from './shopping-list.service';
   selector: 'app-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
   isAdd = true;
-  item: Ingredient;
+  @Input() item: Ingredient;
+  @Output() cleared =  new EventEmitter();
 
   constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
+ngOnChanges(changes) {
+  if (changes.item.currentValue === null){
+    this.item = {name: null, amount: null}
+    this.isAdd = true;
+  } else{
+    this.isAdd = false;
   }
+}
 
   onSubmit(ingredient: Ingredient){
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
     if(!this.isAdd) {
-      //edit
+      this.sls.editItem(this.item, newIngredient)
+      this.onClear();
     } else {
-      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.item = newIngredient;
       this.sls.addItem(this.item);
     }
-
   }
+
+  onDelete(){
+    this.sls.deleteItem(this.item);
+    this.onClear();
+  }
+
+onClear(){
+  this.isAdd = true;
+  this.cleared.emit(null);
+
+}
 
 }
