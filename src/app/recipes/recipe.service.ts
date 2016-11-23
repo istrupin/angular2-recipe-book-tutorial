852ce135cebf13ede0e8c95504c8c86d7f3fb104
Strupinskiy <IStrupin@russellinvestments.com>
2016-11-23 15:58:48 -0800

    true up

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Ingredient}  from '../shared/ingredient';
 import { Recipe } from './recipe';
+import { Headers, Http } from '@angular/http';
 
 
 @Injectable()
@@ -11,7 +12,7 @@ export class RecipeService {
       new Ingredient('orange',1)
     ]),
     new Recipe('Dummy 2','DummyDesc 2', 'http://rishte4u.com/themes/default/assets/images/DummyImage.jpg',[])];
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes;
@@ -31,6 +32,16 @@ export class RecipeService {
 
   editRecipe(oldRecipe:Recipe, newRecipe:Recipe){
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData(){
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({'Content-Type' : 'application/json'});
+    return this.http.post('https://recipebook-b5dcb.firebaseio.com/recipes.json', body, {headers:headers});
+  }
+
+  fetchData(){
+
   }
 
 }
