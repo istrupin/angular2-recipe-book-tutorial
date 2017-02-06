5782208b787e6818e64561badbf2b41cd2df77ad
Strupinskiy <IStrupin@russellinvestments.com>
2017-02-06 12:03:37 -0800

    fixed export problem by removing incorrect import from providers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RecipeService } from './recipes/recipe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  providers: [RecipeService],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    imports: [CommonModule], //don't actually need this import since we are not using it
+    exports: [CommonModule]
+})
+export class SharedModule{ }
